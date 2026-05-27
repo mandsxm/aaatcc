053432d193811e22414c7c17bfaed2ed81481a82
Micaella <micaellagoncalvesbielski@gmail.com>
2026-05-27 11:18:31 -0300

    primeiro commit do projeto

--- tcc/app.py
+++ tcc/app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/tabela')
+def tabela():
+    conexao = mysql.connector.connect(
+        host= 'localhost',
+        port = 3306,    
+        username = 'root',
+        password = '',
+        database = 'almoxarifado'
+    )
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM estoque")
+
+    resultado = cursor.fetchall()
+
+    return render_template('tabela.html', resultado=resultado)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
---
+++ tcc/static/static.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById("Entrar")
+
+botao.addEventListener("click", () => {
+    console.log("teste")
+    window.location.href = "/tabela"
+})
