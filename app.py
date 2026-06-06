9908372ea1e337f1fd9255e23c1239cd56150c17
Amanda <amandalanessouza@gmail.com>
2026-06-06 03:32:45 -0300

    Correção de bugs - versão talvez final

--- app.py
+++ app.py
@@ -1,54 +1,83 @@
-from flask import Flask, render_template, request, jsonify, session, redirect, url_for
+from flask import Flask, render_template, request, jsonify, redirect, session, url_for
 import mysql.connector
 import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.secret_key = 'almoxarifado_secret_key'
+app.secret_key = "chave_secreta_123"
 
-def conectar():
+# CONEXÃO
+
+def get_db():
     return mysql.connector.connect(
         host='localhost',
-        port=3306,
         user='root',
-        password='m4nds@mySQL_',
+        password='',
         database='almoxarifado'
     )
 
-# FUNÇÕES DE AUTORIZAÇÃO
+# AUTORIZAÇÃO
+
 def login_required():
     return 'usuario' in session
-
 
 def admin_required():
     return session.get('tipo') == 'admin'
 
-# DO LOGIN PARA TABELA
-
-# Define a rota inicial
+# HOME 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# ROTAS ADMIN + USUÁRIO
 
-# Tabela.html: Página para visualizar estoque
+# Login da tela do index.html
+
+@app.route('/login', methods=['POST'])
+def login():
+
+    email = request.form.get('email')
+    senha = request.form.get('senha')
+
+    conexao = get_db()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        SELECT * FROM usuarios
+        WHERE email = %s AND senha = %s
+    """, (email, senha))
+
+    user = cursor.fetchone()
+
+    cursor.close()
+    conexao.close()
+
+    if user:
+        session['usuario'] = user[1]
+        session['tipo'] = user[4]
+        session['email'] = user[2]
+        return redirect('/tabela')
+
+    return "Email ou senha inválidos"
+
+# LOGOUT (Ao clicar no botão de logout)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+# TELA TABELA 
 
 @app.route('/tabela')
 def tabela():
+
     if not login_required():
         return redirect('/')
 
-    conexao = mysql.connector.connect(
-        host='localhost',
-        port=3306,
-        user='root',
-        password='m4nds@mySQL_',
-        database='almoxarifado'
-    )
-
+    conexao = get_db()
     cursor = conexao.cursor()
+
     cursor.execute("SELECT * FROM estoque")
     resultado = cursor.fetchall()
 
@@ -57,33 +86,107 @@ def tabela():
 
     return render_template('tabela.html', resultado=resultado)
 
-# Editar.html: Página para editar o estoque
+# ENTRADA / SAÍDA ESTOQUE 
+
+@app.route('/entrada', methods=['POST'])
+def entrada():
+
+    nome = request.form.get('nome')
+    qtde = request.form.get('qtde')
+    responsavel = request.form.get('responsavel')
+    tipo = request.form.get('tipo')
+    imagem = request.files.get("imagem")
+
+    if not nome or not qtde or not responsavel or not tipo:
+        return jsonify({"success": False, "erro": "Campos obrigatórios"}), 400
+
+    qtde = int(qtde)
+
+    caminho_imagem = None
+
+    if imagem:
+        nome_arquivo = secure_filename(imagem.filename)
+
+        pasta = os.path.join("static", "uploads")
+        os.makedirs(pasta, exist_ok=True)
+
+        caminho_salvar = os.path.join(pasta, nome_arquivo)
+        imagem.save(caminho_salvar)
+
+        caminho_imagem = url_for('static', filename=f'uploads/{nome_arquivo}')
+
+    conexao = get_db()
+    cursor = conexao.cursor()
+
+    cursor.execute("SELECT id FROM estoque WHERE nome = %s", (nome,))
+    item = cursor.fetchone()
+
+    if tipo == "entrada":
+
+        if item:
+            cursor.execute("""
+                UPDATE estoque
+                SET qtde = qtde + %s
+                WHERE nome = %s
+            """, (qtde, nome))
+        else:
+            cursor.execute("""
+                INSERT INTO estoque (responsavel, nome, qtde, imagem)
+                VALUES (%s, %s, %s, %s)
+            """, (responsavel, nome, qtde, caminho_imagem))
+
+    elif tipo == "saida":
+
+        cursor.execute("""
+            UPDATE estoque
+            SET qtde = qtde - %s
+            WHERE nome = %s
+        """, (qtde, nome))
+
+    conexao.commit()
+    cursor.close()
+    conexao.close()
+
+    return jsonify({"success": True})
+
+# Excluir item do estoque
+
+@app.route('/excluir/<int:id>', methods=['DELETE'])
+def excluir(id):
+
+    conexao = get_db()
+    cursor = conexao.cursor()
+
+    cursor.execute("DELETE FROM estoque WHERE id = %s", (id,))
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+
+    return jsonify({"success": True})
+
+# TELA EDITAR
+
 @app.route('/editar')
 def editar():
+
     if not login_required():
         return redirect('/')
 
     return render_template('editar.html')
 
-# ROTAS (SOMENTE ADMIN)
+# TELA ACESSO (Apenas para admin)
 
-# Acesso.html: Página para visualizar contas de usuários
 @app.route('/acesso')
 def acesso():
+
     if not login_required():
         return redirect('/')
 
     if not admin_required():
-        return "Acesso negado (somente admin)"
+        return "Acesso negado"
 
-    conexao = mysql.connector.connect(
-        host='localhost',
-        port=3306,
-        user='root',
-        password='m4nds@mySQL_',
-        database='almoxarifado'
-    )
-
+    conexao = get_db()
     cursor = conexao.cursor()
 
     cursor.execute("SELECT * FROM usuarios")
@@ -94,25 +197,15 @@ def acesso():
 
     return render_template('acesso.html', resultado=resultado)
 
-# Excluir usuários
+# Função de excluir usuário (Apenas para admin)
+
 @app.route('/excluirUsuario/<int:id>', methods=['DELETE'])
 def excluir_usuario(id):
 
-    conexao = mysql.connector.connect(
-        host='localhost',
-        port=3306,
-        user='root',
-        password='m4nds@mySQL_',
-        database='almoxarifado'
-    )
-
+    conexao = get_db()
     cursor = conexao.cursor()
 
-    cursor.execute(
-        "DELETE FROM usuarios WHERE id = %s",
-        (id,)
-    )
-
+    cursor.execute("DELETE FROM usuarios WHERE id = %s", (id,))
     conexao.commit()
 
     cursor.close()
@@ -120,154 +213,40 @@ def excluir_usuario(id):
 
     return jsonify({"success": True})
 
-# Cadastrar.html: Página para adicionar e monitorar contas de usuários
-@app.route('/cadastro')
+# TELA CADASTRO DE USUÁRIOS (Apenas para admin)
+
+@app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
+
     if not login_required():
         return redirect('/')
 
     if not admin_required():
-        return "Acesso negado (somente admin)"
+        return "Acesso negado"
 
-    return render_template('cadastro.html')
+    if request.method == 'GET':
+        return render_template('cadastro.html')
 
-@app.route('/entrada', methods=['POST'])
-def entrada():
-
-    nome = request.form.get('nome')
-    qtde = int(request.form.get('qtde'))
-    responsavel = request.form.get('responsavel')
-    tipo = request.form.get('tipo')
-    imagem = request.files.get("imagem")
-
-    conexao = mysql.connector.connect(
-        host='localhost',
-        port=3306,
-        user='root',
-        password='m4nds@mySQL_',
-        database='almoxarifado'
-    )
-
-    cursor = conexao.cursor()
-
- # SALVAR IMAGEM 
-
-    if imagem:
-        nome_arquivo = secure_filename(imagem.filename)
-
-        pasta = "static/uploads"
-        os.makedirs(pasta, exist_ok=True)
-
-        caminho_salvar = os.path.join(pasta, nome_arquivo)
-        imagem.save(caminho_salvar)
-
-        caminho_imagem = "/" + caminho_salvar.replace("\\", "/")
-    else:
-        caminho_imagem = None
-
-# ENTRADA  E SAÍDA DE NOVOS VALORES
-
-    if tipo == "entrada":
-
-        sql = """
-        INSERT INTO estoque (responsavel, nome, qtde, imagem)
-        VALUES (%s, %s, %s, %s)
-        """
-
-        cursor.execute(sql, (responsavel, nome, qtde, caminho_imagem))
-
-    elif tipo == "saida":
-
-        sql = """
-        UPDATE estoque
-        SET qtde = qtde - %s
-        WHERE nome = %s
-        """
-
-        cursor.execute(sql, (qtde, nome))
-
-    conexao.commit()
-    cursor.close()
-    conexao.close()
-
-    return jsonify({"success": True})
-
-# EXCLUIR LINHA
-@app.route('/excluir/<int:id>', methods=['DELETE'])
-def excluir(id):
-
-    conexao = mysql.connector.connect(
-        host='localhost',
-        port=3306,
-        user='root',
-        password='m4nds@mySQL_',
-        database='almoxarifado'
-    )
-
-    cursor = conexao.cursor()
-
-    cursor.execute(
-        "DELETE FROM estoque WHERE id = %s",
-        (id,)
-    )
-
-    conexao.commit()
-
-    cursor.close()
-    conexao.close()
-
-    return jsonify({"success": True})
-
-# LOGIN E LOGOUT + CADASTRO
-@app.route('/login', methods=['POST'])
-def login():
+    usuario = request.form.get('user')
     email = request.form.get('email')
     senha = request.form.get('senha')
+    perfil = request.form.get('perfil')
 
-    conexao = conectar()
-    cursor = conexao.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM usuarios WHERE email = %s AND senha = %s", (email, senha))
-    usuario = cursor.fetchone()
+    conexao = get_db()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        INSERT INTO usuarios (nome, email, senha, tipo)
+        VALUES (%s, %s, %s, %s)
+    """, (usuario, email, senha, perfil))
+
+    conexao.commit()
     cursor.close()
     conexao.close()
 
-    if not usuario:
-        return "Email ou senha inválidos"
+    return redirect('/acesso')
 
-    session['usuario_id'] = usuario['id']
-    session['usuario']    = usuario['user']
-    session['email']      = usuario['email']
-    session['tipo']       = usuario['perfil']
-
-    return redirect('/tabela')
-
-# LOGOUT
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('home'))
-
-# CADASTRO
-@app.route('/cadastro', methods=['POST'])
-def cadastro_post():
-    usuario = request.form.get('user')
-    email   = request.form.get('email')
-    senha   = request.form.get('senha')
-    perfil  = request.form.get('perfil', 'usuario')
-
-    try:
-        conexao = conectar()
-        cursor = conexao.cursor()
-        cursor.execute(
-            "INSERT INTO usuarios (user, email, senha, perfil) VALUES (%s, %s, %s, %s)",
-            (usuario, email, senha, perfil)
-        )
-        conexao.commit()
-        cursor.close()
-        conexao.close()
-        return jsonify({"success": True})
-    except mysql.connector.IntegrityError:
-        return jsonify({"success": False, "mensagem": "E-mail já cadastrado."})
+# RODAR A APLICAÇÃO
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
--- static/static.js
+++ static/static.js
@@ -1,14 +1,30 @@
-// MUDAR PARA PÁGINA TABELA
+// MUDAR PARA TABELA
 const botao = document.getElementById("btn-entrar");
 
 if (botao) {
     botao.addEventListener("click", () => {
-        console.log("teste");
         window.location.href = "/tabela";
     });
 }
 
-// ADICIONAR E RETIRAR ITENS
+// SALVAR + CARREGAR RASCUNHO DA TELA EDITAR
+
+function salvarRascunho() {
+    const item = document.getElementById("item")?.value;
+    const quantidade = document.getElementById("quantidade")?.value;
+    const responsavel = document.getElementById("responsavel")?.value;
+
+    const dados = {
+        item,
+        quantidade,
+        responsavel
+    };
+
+    localStorage.setItem("rascunho_editar", JSON.stringify(dados));
+}
+
+// JS PRINCIPAL
+
 window.addEventListener("DOMContentLoaded", () => {
 
     console.log("JS carregou com sucesso");
@@ -20,237 +36,132 @@ window.addEventListener("DOMContentLoaded", () => {
 
     let tipoMovimentacao = "";
 
-    // FUNÇÃO: limpar seleção
     function limparSelecao() {
-        if (btnEntrada) btnEntrada.classList.remove("btn-selecionado");
-        if (btnSaida) btnSaida.classList.remove("btn-selecionado");
-    }
-
-    // BOTÃO ENTRADA
-    if (btnEntrada) {
-        btnEntrada.addEventListener("click", () => {
-            tipoMovimentacao = "entrada";
-
-            limparSelecao();
-            btnEntrada.classList.add("btn-selecionado");
-
-            console.log("Entrada selecionada");
-        });
-    }
-
-    // BOTÃO SAÍDA
-    if (btnSaida) {
-        btnSaida.addEventListener("click", () => {
-            tipoMovimentacao = "saida";
-
-            limparSelecao();
-            btnSaida.classList.add("btn-selecionado");
-
-            console.log("Saída selecionada");
-        });
+        btnEntrada?.classList.remove("btn-selecionado");
+        btnSaida?.classList.remove("btn-selecionado");
     }
 
 
-// EXCLUIR ITENS
-window.excluirItem = async function(id) {
+    // CARREGAR RASCUNHO DA TELA EDITAR AO ABRIR NOVAMENTE
 
-    const confirmacao = confirm("Tem certeza que deseja excluir este item?");
-    if (!confirmacao) return;
+    const rascunho = JSON.parse(localStorage.getItem("rascunho_editar"));
 
-    const resposta = await fetch(`/excluir/${id}`, {
-        method: "DELETE"
+    if (rascunho) {
+        if (document.getElementById("item"))
+            document.getElementById("item").value = rascunho.item || "";
+
+        if (document.getElementById("quantidade"))
+            document.getElementById("quantidade").value = rascunho.quantidade || "";
+
+        if (document.getElementById("responsavel"))
+            document.getElementById("responsavel").value = rascunho.responsavel || "";
+    }
+
+    // SALVAR ENQUANTO DIGITA
+    document.addEventListener("input", salvarRascunho);
+
+
+    // ENTRADA
+    btnEntrada?.addEventListener("click", () => {
+        tipoMovimentacao = "entrada";
+        limparSelecao();
+        btnEntrada.classList.add("btn-selecionado");
     });
 
-    const data = await resposta.json();
+    // SAÍDA
+    btnSaida?.addEventListener("click", () => {
+        tipoMovimentacao = "saida";
+        limparSelecao();
+        btnSaida.classList.add("btn-selecionado");
+    });
 
-    if (data.success) {
-        alert("Item excluído com sucesso!");
-        location.reload();
-    } else {
-        alert("Erro ao excluir");
-    }
-}
+    // REGISTRAR
+    btnRegistrar?.addEventListener("click", async () => {
 
-// ATUALIZAR A TABELA
-    if (btnRegistrar) {
-        btnRegistrar.addEventListener("click", async () => {
+        if (!tipoMovimentacao) {
+            alert("Selecione Entrada ou Saída.");
+            return;
+        }
 
-            if (tipoMovimentacao === "") {
-                alert("Selecione Entrada ou Saída.");
-                return;
-            }
+        const item = document.getElementById("item").value;
+        const quantidade = document.getElementById("quantidade").value;
+        const responsavel = document.getElementById("responsavel").value;
+        const imagem = document.getElementById("fileInput").files[0];
 
-            const item = document.getElementById("item").value;
-            const quantidade = document.getElementById("quantidade").value;
-            const responsavel = document.getElementById("responsavel").value;
-            const imagem = document.getElementById("fileInput").files[0];
+        const formData = new FormData();
+        formData.append("nome", item);
+        formData.append("qtde", quantidade);
+        formData.append("responsavel", responsavel);
+        formData.append("tipo", tipoMovimentacao);
 
-            const formData = new FormData();
+        if (imagem) {
+            formData.append("imagem", imagem);
+        }
 
-            formData.append("nome", item);
-            formData.append("qtde", quantidade);
-            formData.append("responsavel", responsavel);
-
-            if (imagem) {
-                formData.append("imagem", imagem);
-            }
-
-            formData.append("tipo", tipoMovimentacao);
-
-            const resposta = await fetch("/entrada", {
-                method: "POST",
-                body: formData
-            });
-
-            const data = await resposta.json();
-
-            if (data.success) {
-                alert("Registro salvo com sucesso!");
-                window.location.href = "/tabela";
-            } else {
-                alert("Erro ao salvar");
-            }
+        const resposta = await fetch("/entrada", {
+            method: "POST",
+            body: formData
         });
-    }
 
-    if (btnUpload) {
-        btnUpload.addEventListener("click", () => {
-            document.getElementById("fileInput").click();
+        const data = await resposta.json();
+
+        if (data.success) {
+            alert("Registro salvo com sucesso!");
+
+            // LIMPA RASCUNHO AO SALVAR DE VERDADE
+            localStorage.removeItem("rascunho_editar");
+
+            window.location.href = "/tabela";
+        } else {
+            alert("Erro ao salvar");
+        }
+    });
+
+    // UPLOAD
+    btnUpload?.addEventListener("click", () => {
+        document.getElementById("fileInput").click();
+    });
+
+
+    // EXCLUIR ITEM
+    window.excluirItem = async function(id) {
+
+        const confirmacao = confirm("Tem certeza que deseja excluir este item?");
+        if (!confirmacao) return;
+
+        const resposta = await fetch(`/excluir/${id}`, {
+            method: "DELETE"
         });
-    }
+
+        const data = await resposta.json();
+
+        if (data.success) {
+            alert("Item excluído com sucesso!");
+            location.reload();
+        } else {
+            alert("Erro ao excluir");
+        }
+    };
+
+
+    // EXCLUIR USUÁRIO
+    window.excluirUsuario = async function(id) {
+
+        const confirmacao = confirm("Tem certeza que deseja excluir este usuário?");
+        if (!confirmacao) return;
+
+        const resposta = await fetch(`/excluirUsuario/${id}`, {
+            method: "DELETE"
+        });
+
+        const data = await resposta.json();
+
+        if (data.success) {
+            alert("Usuário excluído com sucesso!");
+            location.reload();
+        } else {
+            alert("Erro ao excluir usuário");
+        }
+    };
 
 });
-
-// ADICIONAR E RETIRAR ITENS
-window.addEventListener("DOMContentLoaded", () => {
-
-    console.log("JS carregou com sucesso");
-
-    const btnEntrada = document.getElementById("btnEntrada");
-    const btnSaida = document.getElementById("btnSaida");
-    const btnRegistrar = document.getElementById("btnRegistrar");
-    const btnUpload = document.getElementById("btnUpload");
-
-    let tipoMovimentacao = "";
-
-    // FUNÇÃO: limpar seleção
-    function limparSelecao() {
-        if (btnEntrada) btnEntrada.classList.remove("btn-selecionado");
-        if (btnSaida) btnSaida.classList.remove("btn-selecionado");
-    }
-
-    // BOTÃO ENTRADA
-    if (btnEntrada) {
-        btnEntrada.addEventListener("click", () => {
-            tipoMovimentacao = "entrada";
-
-            limparSelecao();
-            btnEntrada.classList.add("btn-selecionado");
-
-            console.log("Entrada selecionada");
-        });
-    }
-
-    // BOTÃO SAÍDA
-    if (btnSaida) {
-        btnSaida.addEventListener("click", () => {
-            tipoMovimentacao = "saida";
-
-            limparSelecao();
-            btnSaida.classList.add("btn-selecionado");
-
-            console.log("Saída selecionada");
-        });
-    }
-
-
-// EXCLUIR ITENS
-window.excluirItem = async function(id) {
-
-    const confirmacao = confirm("Tem certeza que deseja excluir este item?");
-    if (!confirmacao) return;
-
-    const resposta = await fetch(`/excluir/${id}`, {
-        method: "DELETE"
-    });
-
-    const data = await resposta.json();
-
-    if (data.success) {
-        alert("Item excluído com sucesso!");
-        location.reload();
-    } else {
-        alert("Erro ao excluir");
-    }
-}
-
-// ATUALIZAR A TABELA
-    if (btnRegistrar) {
-        btnRegistrar.addEventListener("click", async () => {
-
-            if (tipoMovimentacao === "") {
-                alert("Selecione Entrada ou Saída.");
-                return;
-            }
-
-            const item = document.getElementById("item").value;
-            const quantidade = document.getElementById("quantidade").value;
-            const responsavel = document.getElementById("responsavel").value;
-            const imagem = document.getElementById("fileInput").files[0];
-
-            const formData = new FormData();
-
-            formData.append("nome", item);
-            formData.append("qtde", quantidade);
-            formData.append("responsavel", responsavel);
-
-            if (imagem) {
-                formData.append("imagem", imagem);
-            }
-
-            formData.append("tipo", tipoMovimentacao);
-
-            const resposta = await fetch("/entrada", {
-                method: "POST",
-                body: formData
-            });
-
-            const data = await resposta.json();
-
-            if (data.success) {
-                alert("Registro salvo com sucesso!");
-                window.location.href = "/tabela";
-            } else {
-                alert("Erro ao salvar");
-            }
-        });
-    }
-
-    if (btnUpload) {
-        btnUpload.addEventListener("click", () => {
-            document.getElementById("fileInput").click();
-        });
-    }
-
-});
-
-// EXCLUIR USUÁRIOS
-window.excluirUsuario = async function(id) {
-
-    const confirmacao = confirm("Tem certeza que deseja excluir este usuário?");
-    if (!confirmacao) return;
-
-    const resposta = await fetch(`/excluir_usuario/${id}`, {
-        method: "DELETE"
-    });
-
-    const data = await resposta.json();
-
-    if (data.success) {
-        alert("Usuário excluído com sucesso!");
-        location.reload();
-    } else {
-        alert("Erro ao excluir usuário");
-    }
-}
-
